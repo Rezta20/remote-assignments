56d349f016bd57443648958704c05ab1e53276ee
Sasha <mariafu0801@gmail.com>
2020-09-29 12:02:30 +0800

    AS-2 done

--- week-3/node.js
+++ week-3/node.js
@@ -1,32 +1,33 @@
 const express = require("express");
-const https = require("https");
-
 const app = express();
 
 app.get("/",function(req,res){
-    res.send("<h1>Hello, Could you see me?</h1>");
+    res.send("<h1>Hello, Could you see me?</h1>"); 
 });
 
-app.get("/getData",(req, res) =>{
-    res.send("Lack of Parameter");
-});
-
-// webClient webClient = new webClient();
-// webClient.QueryString.Add('number', 'xyz');
-// webClient.QueryString.Add('number', '5');
-// string result = webClient.DownloadString("https//theurl.com");
-
-app.get("/getData?number=xyz",(req,res) =>{
-    res.send("Wrong Parameter");
-});
-
-app.get("`/getData?number=${positiveInteger}`",(req,res) =>{
-    let positiveInteger;
-    let response; 
-    for (let i=0; i< positiveInteger.length; i++){
-        response += positiveInteger[i];
-    }
-    res.send(response);
+app.get("/getData",(req,res) =>{ 
+     // when req.query = {}; req.query = key=value;
+     if(Object.keys(req.query).length == 0){
+         res.send("Lack of Parameter")
+     }
+     // value ->'xyz'
+     else if(Object.values(req.query) == 'xyz'){
+         res.send("Wrong Parameter");
+     }
+     // req.query -> not a number
+     // req.query -> not positive integer -> 0 or negtive number   
+     else if(isNaN(req.query.number) || parseInt(req.query.number) <= 0){
+         res.send('Wrong Parameter');    
+     }
+     // req.qurey.value -> positive integer
+     // each plus together
+     else(Object.values(req.query.number) == parseInt(req.query.number) >0)
+        total = 0;
+        for(let i=1; i<=req.query.number; i++){
+          total = total+i;
+        }
+        res.send(total.toString());    
+     
 });
 
 app.listen(3000, function(){
