fd68473150a8846b084cc63952a33af58eb0a0d7
Sasha <mariafu0801@gmail.com>
2020-10-04 02:02:52 +0800

    week-4 as-2

--- week-4/as-2.js
+++ week-4/as-2.js
@@ -1,26 +1,18 @@
 function ajax(src, callback){
-    fetch(src,{method:'get'}).then(function(response){
-        callback;
-    })
+    fetch(src).then( function(response){
+        response.text().then((t)=>{
+            callback(t);
+        });
+    });
 };
 
 
-
 function render(data){
-
-    data.creatElement();
-    data.appendChild();
+    let response =docuent.createElement('span');
+    response.textContent = data;
+    document.querySelector('body').appendChild(response);
 }
 
-// function ajax(src, callback){
-//     // your code here
-//     }
-//     function render(data){
-//     //callback
-//     // your code here.
-//     // document.createElement() and appendChild() are preferred. No innerHTML or
-//     // something alike
-//     }
 ajax("https://cwpeng.github.io/live-records-samples/data/products.json", function(response){
     render(response);
-    }); // you should get product information in JSON format and render data in the page
+    });
